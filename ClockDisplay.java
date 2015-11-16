d2a95a48678cdbb7b282ad422d117227506d0f63
Manuel <manubp9@gmail.com>
2015-11-16 12:07:01 +0100

    corregido metodo setvalue

---
+++ ClockDisplay.java
@@ -0,0 +1,53 @@
+
+/**
+ * Tener 2 atributos de tipo NumberDisplay, uno para las horas y otro para los minutos.
+Tener un atributo de tipo String donde se almacena la hora actual del reloj con 5 caracteres.
+Tener 2 constructores: uno sin parámetros, que fija la hora a "00:00" y otro con 2 parámetros de tipo int, que representan las horas y los minutos a los que fijar la hora actual.
+Tener un método setTime que acepte dos parámetros de tipo int, que representan horas y minutos, y que fije dichos valores como el tiempo actual del reloj.
+Tener un método getTime que devuelva la hora como String de 5 caracteres.
+Tener un método timeTick que haga avanzar un minuto la hora actual (sea esta la que sea originalmente).
+Una vez que hayas comprobado que funciona, sube el repositorio a GitHub e indica la URL del último commit como respuesta de esta actividad.
+ * 
+ 
+ */
+public class ClockDisplay
+{
+    // instance variables - replace the example below with your own
+    private NumberDisplay horas;
+    private NumberDisplay minutos;
+    private String hora;
+    
+    
+
+    /**
+     * Constructor inicia la hora a 00:00 
+     */
+    public ClockDisplay()
+    {
+       //fija la hora a 00:00
+       horas = new NumberDisplay(23);
+       minutos = new NumberDisplay(59);
+    }
+    /**
+     * constructor para representar horas y minutos
+     */
+    public ClockDisplay(int horasActual, int minutosActual)
+    {
+       horas = new NumberDisplay(23);
+       minutos = new NumberDisplay(59);
+       horas.setValue(horasActual);
+       minutos.setValue(minutosActual);
+       
+       
+    }
+    /**
+     * Tener un método setTime que acepte dos parámetros de tipo int, que representan horas y minutos, 
+     * y que fije dichos valores como el tiempo actual del reloj.
+     */
+    public int setTime(int horas , int minutos){
+        
+    }
+    
+
+
+}
--- NumberDisplay.java
+++ NumberDisplay.java
@@ -5,6 +5,15 @@
      * Un método que devuelve el valor actual del display en forma de cadena de 2 caracteres (nombre del método: getDisplayValue).
      * Un método que devuelve el valor actual del display como entero (nombre del método: getValue),
      * Un método que incrementa en 1 al display y lo hace volver al principio si alcanza el límite (nombre del método: increment)
+     * 
+     * Añade al proyecto donde desarrollaste la clase NumberDisplay una nueva clase llamada ClockDisplay. Esta nueva clase debe:
+
+Tener 2 atributos de tipo NumberDisplay, uno para las horas y otro para los minutos.
+Tener un atributo de tipo String donde se almacena la hora actual del reloj con 5 caracteres.
+Tener 2 constructores: uno sin parámetros, que fija la hora a "00:00" y otro con 2 parámetros de tipo int, que representan las horas y los minutos a los que fijar la hora actual.
+Tener un método setTime que acepte dos parámetros de tipo int, que representan horas y minutos, y que fije dichos valores como el tiempo actual del reloj.
+Tener un método getTime que devuelva la hora como String de 5 caracteres.
+Tener un método timeTick que haga avanzar un minuto la hora actual (sea esta la que sea originalme
      */
 public class NumberDisplay
 {
@@ -25,7 +34,9 @@ public class NumberDisplay
      * setter que fija el valor actual del display al valor pasado como parámetro 
      */
     public void setValue(int value){
-        display = value  ;
+        if (value >= 0 && value < limit)
+        display = value;
+        else  System.out.println (" valor introducido incorrecto");
     }
    
     /**
